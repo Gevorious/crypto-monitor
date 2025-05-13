d8094a804782b066fa4a12aa0363384575f02412
Gevorious <gevorgpahlevanyan@gmail.com>
2025-05-13 12:08:41 +0400

    style changes, hide side column on mobile screen

--- app/monitor/partials/OrdersList.tsx
+++ app/monitor/partials/OrdersList.tsx
@@ -3,18 +3,18 @@ import { OrdersListProps } from '../types';
 const OrdersList = ({ orders, loading }: OrdersListProps) => {
   return (
     <div className="max-h-[75vh] overflow-y-auto px-2 custom-scrollbar">
-      <ul>
+      <ul className="text-center">
         <li className="flex justify-between py-1 border-b border-gray-500 text-gray-300 font-bold">
           <span className="w-[100px]">Time</span>
           <span className="w-[100px]">Symbol</span>
           <span className="w-[100px]">Price</span>
           <span className="w-[100px]">Volume</span>
-          <span className="w-[60px]">Side</span>
+          <span className="w-[50px] hidden sm:block">Side</span>
         </li>
         {orders.map((order) => (
           <li
             key={order.id}
-            className={`flex justify-between py-1 border-b border-gray-700 ${
+            className={`flex justify-between py-1 border-b text-[12px] md:text-[16px] border-gray-700 ${
               order.side === 'Buy' ? 'text-green-400' : 'text-red-400'
             }`}
           >
@@ -22,7 +22,7 @@ const OrdersList = ({ orders, loading }: OrdersListProps) => {
             <span className="w-[100px]">{order.symbol}</span>
             <span className="w-[100px]">${order.price.toFixed(2)}</span>
             <span className="w-[100px]">{order.volume}</span>
-            <span className="w-[60px]">{order.side}</span>
+            <span className="w-[50px] hidden sm:block">{order.side}</span>
           </li>
         ))}
       </ul>
