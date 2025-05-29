a98e570b35edf4f1ac13523f921f599892ca6caa
Gevorious <gevorgpahlevanyan@gmail.com>
2025-05-29 18:57:26 +0400

    fixed order list scroll and static list header

--- app/monitor/partials/OrdersList.tsx
+++ app/monitor/partials/OrdersList.tsx
@@ -8,15 +8,15 @@ const alertingRowMap = {
 
 const OrdersList = ({ orders }: OrdersListProps) => {
   return (
-    <div className="max-h-[75vh] relative overflow-y-auto px-2 custom-scrollbar text-center text-[10px] md:text-[16px]">
-      <div className="flex fixed left-0 right-0 justify-between bg-black z-1 border-b border-gray-500 py-1 mx-6 text-gray-300 font-bold">
+    <div className="px-2 text-center text-[10px] md:text-[16px]">
+      <div className="flex justify-between border-b border-gray-500 pl-1 pr-2 text-gray-300 font-bold">
         <span className="w-[100px]">Time</span>
         <span className="w-[100px]">Symbol</span>
         <span className="w-[100px]">Price</span>
         <span className="w-[100px]">Volume</span>
         <span className="w-[50px]">Side</span>
       </div>
-      <ul className="mt-8">
+      <ul className="overflow-y-scroll custom-scrollbar max-h-[75vh]">
         {orders.map((order, i) => (
           <li
             key={`order-${i}`}
